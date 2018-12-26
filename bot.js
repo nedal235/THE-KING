c85302a64fb12d70a30ce8c06a5291e8c9f4301a
nedal235 <nedalmaged235@gmail.com>
2018-12-26 15:31:43 +0200

    u

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const discord = require("discord.js");
 const { Client, Util } = require('discord.js');
 const { PREFIX, GOOGLE_API_KEY } = require('./config');
+const botconfig = require("./config");
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 var opus = require('opusscript');
-const bot = new discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 const client = new Client({ disableEveryone: true });
 
 const youtube = new YouTube(GOOGLE_API_KEY);
@@ -105,7 +106,7 @@ Please provide a value to select one of the search results ranging from 1-10.
 		return msg.channel.send(`I set the volume to: **${args[1]}**`);
 	} else if (command === 'np') {
 		if (!serverQueue) return msg.channel.send('There is nothing playing.');
-		return msg.channel.send(`ðŸŽ¶ Now playing: **${serverQueue.songs[0].title}**`);
+		return msg.channel.send(`Now playing: **${serverQueue.songs[0].title}**`);
 	} else if (command === 'queue') {
 		if (!serverQueue) return msg.channel.send('There is nothing playing.');
 		return msg.channel.send(`
@@ -126,7 +127,7 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
 		if (serverQueue && !serverQueue.playing) {
 			serverQueue.playing = true;
 			serverQueue.connection.dispatcher.resume();
-			return msg.channel.send('â–¶ Resumed the music for you!');
+			return msg.channel.send('Resumed the music for you!');
 		}
 		return msg.channel.send('There is nothing playing.');
 	}
@@ -193,8 +194,171 @@ function play(guild, song) {
 		.on('error', error => console.error(error));
 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
 
-	serverQueue.textChannel.send(`ðŸŽ¶ Start playing: **${song.title}**`);
-}
-client.login(process.env.BOT_TOKEN);
+	serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+        }
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
 
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if(message.content.includes('discord.gg')){
+        message.delete();
+      message.channel.sendMessage("", {embed: {
+        title: "لا تنشر",
+        color: 0x06DF00,
+        description: "يمنع النشر في هذا السيرفر",
+        footer: {
+          text: "تم مسح الرسالة"
+        }
+      }}).then(msg => {msg.delete(3000);});
+                          }
+if(cmd === `${prefix}ic`){
+
+    var ic = argresult;
+
+    return message.channel.send("Done");
+  }
+
+  if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, ic);
+    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+    return;
+  }
+
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("Can't find user!");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("you cant!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("~Ban~")
+    .setColor("#bc0000")
+    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
+    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, ic);
+    if(!incidentchannel) return message.channel.send("Can't find incidents channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
+
+
+    return;
+  }
+
+  if(cmd === `${prefix}serverinfo`){
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+
+  if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
+  }
+  
+  if(cmd === `${prefix}st`){
+
+    bot.user.setGame(argresult , "https://www.twitch.tv/ninja");
+
+    return message.channel.send("Done");
+  }
+ 
+  if(cmd === `${prefix}ls`){
+
+    bot.user.setActivity(argresult , {type:'LISTENING'});
+
+    return message.channel.send("Done");
+  }
+   
+  if(cmd === `${prefix}wt`){
+
+    bot.user.setActivity(argresult , {type:'WATCHING'});
+
+    return message.channel.send("Done");
+  }
+ 
+  if(cmd === `${prefix}pl`){
+
+    bot.user.setGame(argresult);
+
+    return message.channel.send("Done");
+  }
+
+  if(cmd === `${prefix}lockchannel`) {
+                        if(!message.channel.guild) return message.reply(' This command only for servers');
+ 
+     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+ 
+           }).then(() => {
+               message.reply("تم تقفيل الشات :white_check_mark: ");
+           });
+             }
+ 
+  if(cmd === `${prefix}unlock`) {
+    if(!message.channel.guild) return message.reply(' This command only for servers');
+ 
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+ 
+           }).then(() => {
+               message.reply("تم فتح الشات:white_check_mark:");
+           });
+             }
+
+
+});
+client.login(process.env.BOT_TOKEN);
 
