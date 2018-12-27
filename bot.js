d9780cce4637fd494b7ecc3d2aa915cd1c8ceda0
nedal235 <nedalmaged235@gmail.com>
2018-12-27 14:52:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,6 +244,30 @@ client.on("message", async message => {
 
     return;
   }
+  if(cmd === `${prefix}mute`) {
+let reason = args.slice(1).join(' ');
+let user = message.mentions.users.first();
+if (!message.member.hasPermission("MUTE_MEMBERS")) return;
+message.reply(":no_entry_sign: **Error:** You don't have the **Mute Members** permission!");
+if (reason.length < 1) return message.reply('You must supply a reason for the mute.');
+if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+
+if (!message.guild.member(user).bannable) return; 
+message.reply(`:no_entry_sign: I cannot mute that member`);
+message.guild.member(user).ban();
+
+const embed = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .addField('Action:', 'Mute')
+  .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+  .addField('Moderator:', 
+  `${message.author.username}#${message.author.discriminator}`)
+  .addField('Reason', reason);
+  message.channel.send(`:hammer: Bippity boppity **MUTED**! I\'ve logged the mute in the logs channel.`);
+  return message.channel.send(embed);
+  };
+
 
   if(cmd === `${prefix}serverinfo`){
 
